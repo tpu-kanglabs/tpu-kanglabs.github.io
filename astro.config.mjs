2ba6e548f7d8e6a7fb517b329981b0e4c0d0a8f3
Ryota Murai <35632215+rmuraix@users.noreply.github.com>
2026-07-11 06:57:06 +0900

    feat: add sitemap integration (#117)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 
 import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tpu-kanglabs.github.io",
   compressHTML: true,
   build: {
     inlineStylesheets: "always",
@@ -18,5 +20,9 @@ export default defineConfig({
     },
   },
 
-  integrations: [react(), partytown()],
+  integrations: [
+    react(),
+    partytown(),
+    sitemap({ customPages: ["https://tpu-kanglabs.github.io/ub-moji/"] }),
+  ],
 });
